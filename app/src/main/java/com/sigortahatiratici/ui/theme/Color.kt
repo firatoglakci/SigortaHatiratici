e84d017ca9b1d271338b7b7f362a795e6a4e3d80
firatoglakci <47206462+firatoglakci@users.noreply.github.com>
2026-02-20 09:30:12 +0300

    Create Color.kt

---
+++ app/src/main/java/com/sigortahatiratici/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.sigortahatiratici.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val MaviPrimary = Color(0xFF1976D2)
+val MaviSecondary = Color(0xFF42A5F5)
+val MaviTertiary = Color(0xFF90CAF9)
+val KirmiziHata = Color(0xFFD32F2F)
+val TuruncuUyari = Color(0xFFF57C00)
+val YesilBasarili = Color(0xFF388E3C)
